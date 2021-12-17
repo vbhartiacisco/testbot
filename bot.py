cf78ee1bbd9956c49d64081457dc1c4e4a618e65
Vedansh Bhartia <vbhartia@cisco.com>
2021-12-17 12:20:20 +0530

    Initial commit

---
+++ bot.py
@@ -0,0 +1,57 @@
+"""
+Copyright (c) 2021 Cisco and/or its affiliates.
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+               https://developer.cisco.com/docs/licenses
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+"""
+
+from flask import Flask, request, jsonify
+from webexteamssdk import WebexTeamsAPI
+import os
+
+# get environment variables
+WT_BOT_TOKEN = "ZjA3ZTg4YWItM2I0OS00ZGRmLTkwOGUtZmJhODFjMWZlZGI3ZTFjZTM3NTEtZGJh_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
+
+# uncomment next line if you are implementing a notifier bot
+#WT_ROOM_ID = os.environ['WT_ROOM_ID']
+
+uncomment next line if you are implementing a controller bot
+WT_BOT_EMAIL = "testingbot23@webex.bot"
+
+# start Flask and WT connection
+app = Flask(__name__)
+api = WebexTeamsAPI(access_token=WT_BOT_TOKEN)
+
+
+# defining the decorater and route registration for incoming alerts
+@app.route('/', methods=['GET, POST'])
+def alert_received():
+    raw_json = request.get_json()
+    print(raw_json)
+
+    # customize the behaviour of the bot here
+    message = "Hi, I am a Webex Teams bot. Have a great day ☀! "
+
+    # uncomment if you are implementing a notifier bot
+    '''
+    api.messages.create(roomId=WT_ROOM_ID, markdown=message)
+    '''
+
+
+    # uncomment if you are implementing a controller bot
+    WT_ROOM_ID = raw_json['data']['roomId']
+    personEmail_json = raw_json['data']['personEmail']
+    if personEmail_json != WT_BOT_EMAIL:
+        api.messages.create(roomId=WT_ROOM_ID, markdown=message)
+
+    return jsonify({'success': True})
+
+if __name__=="__main__":
+    app.run()
